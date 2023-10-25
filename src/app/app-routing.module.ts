06e20e349c2bac051ef6ca97cbe49af5b6576eb9
camilo andres lemos <camilolemus30@gmail.com>
2023-10-24 22:14:33 -0500

    validar-para dar permiso

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EspecialidadIndexComponent } from './especialidades/especialidad-index/especialidad-index.component';
 import { EspecialidadFormComponent } from './especialidades/especialidad-form/especialidad-form.component';
+import { EspecialidadGuard } from './utils/especialidad.guard';
 
 const routes: Routes = [
-  {path: 'especialidades', component: EspecialidadIndexComponent},
+  {path: 'especialidades', component: EspecialidadIndexComponent,canActivate: [EspecialidadGuard]},
   {path: 'especialidades/nuevo', component: EspecialidadFormComponent},
   {path: 'especialidades/editar/:id',component: EspecialidadFormComponent}
 ];
